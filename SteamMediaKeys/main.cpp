ae5feb92bb0ece421f15201b29fb78edf7ab025e
SE2Dev <brokensilencedev@gmail.com>
2015-10-09 17:44:13 -0400

    Add SteamAPI Initialization

--- SteamMediaKeys/main.cpp
+++ SteamMediaKeys/main.cpp
@@ -1,6 +1,42 @@
 #include <Windows.h>
 
+#pragma warning(push, 0)  
+#include "sdk\public\steam\steam_api.h"
+#pragma comment(lib, "sdk\\redistributable_bin\\steam_api.lib")
+#pragma warning(pop)  
+
+#define STEAMAPI_STEAMISRUNNING_RETRY_COUNT 64
+#define STEAMAPI_STEAMISRUNNING_RETRY_DELAY 1000
+
+static ISteamMusic* IMus = nullptr;
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	bool SteamAPI_SteamIsRunning = SteamAPI_IsSteamRunning();
+
+	for(int i = 0; !SteamAPI_SteamIsRunning && i < STEAMAPI_STEAMISRUNNING_RETRY_COUNT; i++)
+	{
+		SteamAPI_SteamIsRunning = SteamAPI_IsSteamRunning();
+		Sleep(STEAMAPI_STEAMISRUNNING_RETRY_DELAY);
+	}
+
+	if(!SteamAPI_SteamIsRunning)
+	{
+		MessageBox(0,L"ERROR: Could not initialize SteamAPI (Steam is not running)", 0, 0);
+		return 1;
+	}
+
+	if(!SteamAPI_Init())
+	{
+		MessageBox(0, L"ERROR: Could not initialize SteamAPI", 0, 0);
+		return 2;
+	}
+
+	if(!(IMus = SteamMusic()))
+	{
+		MessageBox(0, L"ERROR: Could not initialize Steam Music", 0, 0);
+		return 3;
+	}
+
 	return 0;
 }
